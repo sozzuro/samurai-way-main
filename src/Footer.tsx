0d9e5661a536f18f9c229d37cad1ba6467108131
Denis Radivil <sozzuro@gmail.com>
2022-02-27 15:11:00 +0300

    feat: create Footer Component

---
+++ src/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <div>
+            samurai-way
+        </div>
+    )
+}
+
+export default Footer;
--- src/Header.tsx
+++ src/Header.tsx
@@ -3,9 +3,9 @@ import React from "react";
 function Header() {
     return (
         <div>
-            <a href="">Home</a>
-            <a href="">News Feed</a>
-            <a href="">Messages</a>
+            <a href="">Home</a> -
+            <a href="">News Feed</a> -
+            <a href="">Messages</a> - 
         </div>
     )
 }
