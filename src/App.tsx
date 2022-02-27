53e10a4e4a3db554864add22be475e1f51e770e5
Denis Radivil <sozzuro@gmail.com>
2022-02-27 14:22:04 +0300

    feat: create Header and Technologies components

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,35 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+            <Technologies />
+        </div>
+    );
+}
+
+function Technologies() {
+    return (
+        <div>
+            <ul>
+                <li>css</li>
+                <li>html</li>
+                <li>js</li>
+                <li>react</li>
+            </ul>
+        </div>
+    )
+}
+
+function Header() {
+    return (
+        <div>
+            <a href="">Home</a>
+            <a href="">News Feed</a>
+            <a href="">Messages</a>
+        </div>
+    )
 }
 
 export default App;
