5a342dd06a0ac346e99a90541e206fd34e0e6844
Denis Radivil <sozzuro@gmail.com>
2022-02-27 21:50:55 +0300

    feat: styling Dialogs page

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -3,8 +3,19 @@ import styles from './Dialogs.module.css'
 
 function Dialogs() {
     return (
-        <div>
-            Dialogs
+        <div className={styles.dialogs}>
+            <div className={styles.dialogsItems}>
+                <div className={`${styles.dialog} ${styles.active}`}>Dimych</div>
+                <div className={styles.dialog}>Andrey</div>
+                <div className={styles.dialog}>Sveta</div>
+                <div className={styles.dialog}>Sasha</div>
+                <div className={styles.dialog}>Valera</div>
+            </div>
+            <div className={styles.messages}>
+                <div className={styles.message}>Hi</div>
+                <div className={styles.message}>How are you?</div>
+                <div className={styles.message}>I'm fine!</div>
+            </div>
         </div>
     )
 }
