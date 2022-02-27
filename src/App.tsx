cdbcec606486d2ff7fa77406fba2bcc29cce002c
Denis Radivil <sozzuro@gmail.com>
2022-02-27 21:13:24 +0300

    feat: add browser-router

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,26 @@ import Navbar from "./components/Navbar/Navbar";
 import Profile from "./components/Profile/Profile";
 import Header from "./components/Header/Header";
 import Dialogs from "./components/Dialogs/Dialogs";
+import {BrowserRouter, Route} from "react-router-dom";
+import Music from "./components/Music/Music";
+import News from "./components/News/News";
+import Settings from "./components/Settings/Settings";
 
 function App() {
     return (
-        <div className='app-wrapper'>
-            <Header/>
-            <Navbar/>
-            <div className='app-wrapper-content'>
-                <Dialogs/>
+        <BrowserRouter>
+            <div className='app-wrapper'>
+                <Header/>
+                <Navbar/>
+                <div className='app-wrapper-content'>
+                    <Route path='/profile' component={Profile} />
+                    <Route path='/dialogs' component={Dialogs} />
+                    <Route path='/music' component={Music} />
+                    <Route path='/news' component={News} />
+                    <Route path='/settings' component={Settings} />
+                </div>
             </div>
-            {/*<Profile/>*/}
-        </div>
+        </BrowserRouter>
     );
 }
 
