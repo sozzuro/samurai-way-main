5852d333d8e6dc67e046186e9b6ab778d2ac5f5d
Denis Radivil <sozzuro@gmail.com>
2022-02-27 16:19:00 +0300

    feat: create Header, Navbar, Profile components

---
+++ src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+    return (
+        <header className='header'>
+            <img src="https://images.ru.prom.st/653840423_w600_h600_zachem-kompanii-nuzhen.jpg" alt="logo"/>
+        </header>
+    )
+}
+
+export default Header;
