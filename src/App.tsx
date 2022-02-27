7ffbfd91cf88e5958702d355d6864e1f60075905
Denis Radivil <sozzuro@gmail.com>
2022-02-27 22:08:03 +0300

    feat: add exact route

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
                 <Navbar/>
                 <div className='app-wrapper-content'>
                     <Route path='/profile' component={Profile} />
-                    <Route path='/dialogs' component={Dialogs} />
+                    <Route exact path='/dialogs' component={Dialogs} />
                     <Route path='/music' component={Music} />
                     <Route path='/news' component={News} />
                     <Route path='/settings' component={Settings} />
--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,15 +1,29 @@
 import React from "react";
+import {NavLink} from "react-router-dom";
 import styles from './Dialogs.module.css'
 
 function Dialogs() {
     return (
         <div className={styles.dialogs}>
             <div className={styles.dialogsItems}>
-                <div className={`${styles.dialog} ${styles.active}`}>Dimych</div>
-                <div className={styles.dialog}>Andrey</div>
-                <div className={styles.dialog}>Sveta</div>
-                <div className={styles.dialog}>Sasha</div>
-                <div className={styles.dialog}>Valera</div>
+                <div className={`${styles.dialog} ${styles.active}`}>
+                    <NavLink to='/dialogs/1'>Dimych</NavLink>
+                </div>
+                <div className={styles.dialog}>
+                    <NavLink to='/dialogs/2'>Andrey</NavLink>
+                </div>
+                <div className={styles.dialog}>
+                    <NavLink to='/dialogs/3'>Sveta</NavLink>
+                </div>
+                <div className={styles.dialog}>
+                    <NavLink to='/dialogs/4'>Sasha</NavLink>
+                </div>
+                <div className={styles.dialog}>
+                    <NavLink to='/dialogs/5'>Viktor</NavLink>
+                </div>
+                <div className={styles.dialog}>
+                    <NavLink to='/dialogs/6'>Valera</NavLink>
+                </div>
             </div>
             <div className={styles.messages}>
                 <div className={styles.message}>Hi</div>
