35ba486625d48cec419fbb9888bbcb7d474ea175
Denis Radivil <sozzuro@gmail.com>
2022-02-27 15:50:29 +0300

    feat: create structure of app Samurai way with grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,41 @@
 import React from 'react';
 import './App.css';
-import Technologies from "./Technologies";
-import Header from "./Header";
-import Footer from "./Footer";
 
 function App() {
     return (
-        <div>
-            <Header />
-            <Technologies />
-            <Footer />
+        <div className='app-wrapper'>
+            <header className='header'>
+                <img src="https://images.ru.prom.st/653840423_w600_h600_zachem-kompanii-nuzhen.jpg" alt="logo"/>
+            </header>
+            <nav className='nav'>
+                <div>Profile</div>
+                <div>Messages</div>
+                <div>News</div>
+                <div>Music</div>
+                <div>Settings</div>
+            </nav>
+            <div className='content'>
+                <div>
+                    <img src="https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=350" alt="image"/>
+                </div>
+                <div>
+                    ava + description
+                </div>
+                <div>
+                    My posts
+                    <div>
+                        New post
+                    </div>
+                    <div>
+                        <div>
+                            post 1
+                        </div>
+                        <div>
+                            post 2
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
